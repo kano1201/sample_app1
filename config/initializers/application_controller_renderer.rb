340564403d15eb6d33ba36f73528762421f04677
kano1201 <84449468+kano1201@users.noreply.github.com>
2021-06-06 20:55:43 +0900

    Add files via upload

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '3ff80c37191a81e7a0342ac8cd4dbcbdc1787388c6ebc51c4a7563162188f82a62dd73674a2b62b22aba5692a56bf07c88088acdde2fccf8f6220edc3809c6f0'
